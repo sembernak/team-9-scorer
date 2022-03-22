722686e6de5369eb193fb2be967a80a7ebe18e62
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-03-22 19:02:37 -0400

    added welcome message and button to count

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [value, setValue] = useState<number>(0);
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                CISC275: WELCOME TO GROUP 9 WEBSITE
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <span>
+                <Button onClick={() => setValue(1 + value)}>Add One</Button>
+            </span>
+            <span>
+                <Button onClick={() => setValue(value - 1)}>
+                    Subtract One
+                </Button>
+                Count = {value}
+            </span>
         </div>
     );
 }
